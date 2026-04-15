4568f9d73db0de56777fc301330a2d6d2f033d0d
DevEnrqiue <luiscuyap@gmail.com>
2026-04-15 11:21:23 -0500

    feat: add notfoundexception and crud getUserById

--- src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -3,10 +3,13 @@ package net.javaguides.ems.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems.dto.EmployeeDto;
 import net.javaguides.ems.entity.Employee;
+import net.javaguides.ems.exception.ResourceNotFoundException;
 import net.javaguides.ems.mapper.EmployeeMapper;
 import net.javaguides.ems.repository.EmployeeRepository;
 import net.javaguides.ems.service.EmployeeService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -19,5 +22,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(long userId) {
+        Employee employee = employeeRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee is not exist with given id : " + userId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
